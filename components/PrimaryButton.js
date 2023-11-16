6efce19f5b92558952e32b93f13aee518e1f778f
Mauricio Mercado <mau@maumercado.com>
2023-11-16 15:27:36 -0800

    Add basic functionality to startGameScreen

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -1,15 +1,11 @@
 import { View, Text, Pressable, StyleSheet } from 'react-native'
 
-function PrimaryButton ({ children }) {
-  function pressHandler () {
-    console.log('PrimaryButton pressed')
-  }
-
+function PrimaryButton ({ children, onPress }) {
   return (
     <View style={styles.buttonsOuterContainer}>
       <Pressable
         style={({ pressed }) => pressed ? [styles.buttonInnerContainer, styles.pressed] : styles.buttonInnerContainer}
-        onPress={pressHandler}
+        onPress={onPress}
         android_ripple={{ color: '#840344' }}
       >
         <Text style={styles.buttonText}>{children}</Text>
